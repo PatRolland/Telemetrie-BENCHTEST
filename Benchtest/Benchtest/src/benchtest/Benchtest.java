bbc0ef8b2b9fcba89aff12e8581e6b59e5194dd6
PatRolland <pat.rolland10@gmail.com>
2013-12-29 14:21:56 -0500

    2e

--- Benchtest/Benchtest/src/benchtest/Benchtest.java
+++ Benchtest/Benchtest/src/benchtest/Benchtest.java
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 
-package javafxapplication_test;
+package benchtest;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -19,15 +18,12 @@ import javafx.stage.Stage;
  *
  * @author Patrice
  */
-public class JavaFXApplication_test extends Application {
+public class Benchtest extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();  
-        Button btn2 = new Button();
-        btn2.setOpacity(0f);
+        Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn2.setText("btn2 in da house!");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
@@ -35,25 +31,15 @@ public class JavaFXApplication_test extends Application {
                 System.out.println("Hello World!");
             }
         });
-        btn.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            
-            @Override
-            public void handle(ContextMenuEvent t) {
-                System.out.println("Fuck You World!");
-            }
-        });
-        
         
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        root.getChildren().add(btn2);
         
         Scene scene = new Scene(root, 300, 250);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
-        
     }
 
     /**
